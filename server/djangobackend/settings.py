b87e87d21ac5cce6f674cc5b7cec289e8f5a50ca
thomaskrug96 <thomaskrug1996@gmail.com>
2022-04-10 14:26:50 -0400

    update static files dir for deployment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
